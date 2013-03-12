b7a23c7eee91992dabb0c90f04e50b3401a3ca38
tshoeb <talal@cmu.edu>
2013-03-12 23:02:03 +0300

    fixing errors

--- db/migrate/20130224105534_create_registrars.rb
+++ db/migrate/20130224105534_create_registrars.rb
@@ -5,6 +5,7 @@ class CreateRegistrars < ActiveRecord::Migration
       t.string :last_name
       t.string :email
       t.integer :phone
+      t.integer :schedule_id
 
       t.timestamps
     end
