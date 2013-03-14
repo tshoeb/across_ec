6a55106375e35a84660d812c6b02211ffeb14163
tshoeb <talal@cmu.edu>
2013-03-14 10:45:31 +0300

    fixing again

--- db/migrate/20130314072635_remove_schedule_id_from_registrar.rb
+++
@@ -1,9 +0,0 @@
-class RemoveScheduleIdFromRegistrar < ActiveRecord::Migration
-  def up
-    remove_column :registrars, :schedule_id
-  end
-
-  def down
-    add_column :registrars, :schedule_id, :integer
-  end
-end
