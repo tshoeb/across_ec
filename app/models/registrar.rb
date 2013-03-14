94e39667a753953c8f4e661fd1ab55a74996a1b3
tshoeb <talal@cmu.edu>
2013-03-14 17:41:45 +0300

    final

--- app/models/registrar.rb
+++ app/models/registrar.rb
@@ -7,6 +7,8 @@ class Registrar < ActiveRecord::Base
   has_many :schedules
   belongs_to :university
   validates :first_name, :last_name, :email, :phone, :presence => true
+   validates_format_of :first_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid Name"
+  validates_format_of :last_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid Name"
   validates_format_of :email, :with => /^[\w]([^@\s,;]+)@([\w-]+\.)+(com|edu|org|gov|net|mil|biz|info|qa)$/i, :message => "not vaild email" 
   validates_format_of :phone, :with => /^(\+?\d{11}|\+?\d{3}?[-.]\d{4}?[-.]\d{4})$/, :message => "should be 11 digits (country code needed)"
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -6,4 +6,10 @@ class Student < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :major, :mobile, :student_number, :year_of_graduation, :password, :password_confirmation, :university_id
 
   has_many :applications
+
+  validates :first_name, :last_name, :email, :mobile, :presence => true
+  validates_format_of :first_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid Name"
+  validates_format_of :last_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid Name"
+  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@([\w-]+\.)+(com|edu|org|gov|net|mil|biz|info|qa)$/i, :message => "not vaild email" 
+  validates_format_of :mobile, :with => /^(\+?\d{11}|\+?\d{3}?[-.]\d{4}?[-.]\d{4})$/, :message => "should be 11 digits (country code needed)"
 end
