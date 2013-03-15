24c568aff451699dc99fc6fc0064fdf0a24832fc
tshoeb <talal@cmu.edu>
2013-03-15 22:42:40 +0300

    email regex and other validations added

--- app/models/student.rb
+++ app/models/student.rb
@@ -8,8 +8,9 @@ class Student < ActiveRecord::Base
   has_many :applications
 
   validates :first_name, :last_name, :email, :mobile, :presence => true
-  validates_format_of :first_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid Name"
-  validates_format_of :last_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid Name"
-  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@([\w-]+\.)+(com|edu|org|gov|net|mil|biz|info|qa)$/i, :message => "not vaild email" 
+  validates_format_of :first_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid First Name"
+  validates_format_of :last_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid Last Name"
+  validates_format_of :major, :with => /^[a-z A-Z -']+$/, :message => "Invalid Major"
+  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(qatar\.)?(cmu|vcu|tamu|georgetown)\.(edu)$/i, :message => "not vaild email" 
   validates_format_of :mobile, :with => /^(\+?\d{11}|\+?\d{3}?[-.]\d{4}?[-.]\d{4})$/, :message => "should be 11 digits (country code needed)"
 end
