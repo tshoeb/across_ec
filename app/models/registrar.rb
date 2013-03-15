c14741fbedfd9cf7de15bd661d45bd222850e90f
tshoeb <talal@cmu.edu>
2013-03-15 22:43:31 +0300

    email regex added for registrar

--- app/models/registrar.rb
+++ app/models/registrar.rb
@@ -9,6 +9,6 @@ class Registrar < ActiveRecord::Base
   validates :first_name, :last_name, :email, :phone, :presence => true
    validates_format_of :first_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid Name"
   validates_format_of :last_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid Name"
-  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@([\w-]+\.)+(com|edu|org|gov|net|mil|biz|info|qa)$/i, :message => "not vaild email" 
+  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(qatar\.)?(cmu|vcu|tamu|georgetown)\.(edu)$/i, :message => "not vaild email" 
   validates_format_of :phone, :with => /^(\+?\d{11}|\+?\d{3}?[-.]\d{4}?[-.]\d{4})$/, :message => "should be 11 digits (country code needed)"
 end
