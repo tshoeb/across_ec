2b98f3fe897bafb667f1f636d91686ea3785e14b
Talal Shoeb <talal@cmu.edu>
2013-04-03 10:55:46 +0300

    fixing mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 AcrossEc::Application.initialize!
+
+config.action_mailer.delivery_method = :smtp
+  
+ config.action_mailer.default_url_options = { :host => "across-ec.herokuapp.com" }
+
+ config.action_mailer.smtp_settings = {
+	:enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => 'plain',
+    :user_name => 'across.ec',
+    :password => 'across_ec123'
+  }
