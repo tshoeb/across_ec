922bca96c175cf1b8b2fac9a4f838ea739de4666
Talal Shoeb <talal@cmu.edu>
2013-04-03 10:36:49 +0300

    changes to mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ AcrossEc::Application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-	:enable_starttls_auto: true,
+	:enable_starttls_auto => true,
     :address => "smtp.gmail.com",
     :port => 587,
     :authentication => 'plain',
