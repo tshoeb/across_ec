04ea5f5b931cd4a247ef5dcaf141d02273bef047
Talal Shoeb <talal@cmu.edu>
2013-04-12 13:07:10 +0300

    previous thing fixed for registrar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 	    session[:registrar_id] = registrar.id
 	    redirect_to root_url, :notice => "Logged in!"
 	  else
-	    flash.now.alert = "Invalid email or password"
+	    flash.now.notice = "Invalid email or password"
 	    render "new"
 	  end
 	end
