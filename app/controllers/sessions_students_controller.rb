6be25e7b9cf1f72b573bff456b3357c671dc1f7c
Talal Shoeb <talal@cmu.edu>
2013-04-12 13:06:24 +0300

    fixed the flash notice if added wrong email password for login

--- app/controllers/sessions_students_controller.rb
+++ app/controllers/sessions_students_controller.rb
@@ -8,7 +8,7 @@ class SessionsStudentsController < ApplicationController
 	    session[:student_id] = student.id
 	    redirect_to root_url, :notice => "Logged in!"
 	  else
-	    flash.now.alert = "Invalid email or password"
+	    flash.now.notice = "Invalid email or password"
 	    render "new"
 	  end
   end
