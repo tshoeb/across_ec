6fda6618f49e61c608769e7c46ff6b118e960e68
Talal Shoeb <talal@cmu.edu>
2013-04-21 01:39:16 +0300

    ready for usability

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ActiveRecord::Base
 
-  #before_create :set_confirmation_code
+  before_create :set_confirmation_code
   has_secure_password
   validates_presence_of :password, :on => :create
 
@@ -15,7 +15,7 @@ class Student < ActiveRecord::Base
   validates_format_of :first_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid First Name"
   validates_format_of :last_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid Last Name"
   validates_format_of :major, :with => /^[a-z A-Z -']+$/, :message => "Invalid Major"
-  #validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(qatar\.)?(cmu|vcu|tamu|georgetown)\.(edu)$/i, :message => "not vaild email" 
+  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(qatar\.)?(cmu|vcu|tamu|georgetown)\.(edu)$/i, :message => "not vaild email" 
   validates_format_of :mobile, :with => /^(\+?\d{8}|\+?\d{3}?[-.]\d{4}?[-.]\d{4})$/, :message => "should be 8 digits"
 
   def proper_name
