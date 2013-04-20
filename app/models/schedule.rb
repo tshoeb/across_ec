9a87e96524e7314193632d86f2dc366622443bae
Talal Shoeb <talal@cmu.edu>
2013-04-21 01:44:24 +0300

    schedule date selection fixed

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,13 @@
 class Schedule < ActiveRecord::Base
+
+  before_save :setting_date
+
   attr_accessible :date_uploaded, :file_name, :schedule, :semester_year, :registrar_id
   mount_uploader :schedule, ScheduleUploader
   belongs_to :registrar
   validates :file_name, :date_uploaded, :semester_year, :presence => true  
+  
+  def setting_date
+	self.date_uploaded = Date.today
+  end
 end
