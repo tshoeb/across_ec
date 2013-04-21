a367b6d2ed41e0e3782c7f3aac4b5976cb21ab26
Talal Shoeb <talal@cmu.edu>
2013-04-22 00:01:10 +0300

    added cornell email format

--- app/models/registrar.rb
+++ app/models/registrar.rb
@@ -9,7 +9,7 @@ class Registrar < ActiveRecord::Base
   validates :first_name, :last_name, :email, :phone, :presence => true
    validates_format_of :first_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid Name"
   validates_format_of :last_name, :with => /^[a-z A-Z \- -']+$/, :message => "Invalid Name"
-  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(qatar|qatar-med\.)?(cmu|vcu|tamu|georgetown|cornell)\.(edu)$/i, :message => "not vaild email" 
+  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(qatar\.|qatar-med\.)?(cmu|vcu|tamu|georgetown|cornell)\.(edu)$/i, :message => "not vaild email" 
   validates_format_of :phone, :with => /^(\+?\d{8}|\+?\d{3}?[-.]\d{4}?[-.]\d{4})$/, :message => "should be 11 digits (country code needed)"
   
   def proper_name
--- app/models/student.rb
+++ app/models/student.rb
@@ -13,9 +13,9 @@ class Student < ActiveRecord::Base
   validates :first_name, :last_name, :email, :mobile, :presence => true
   validates :email, :uniqueness => true
   validates_format_of :first_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid First Name"
-  validates_format_of :last_name, :with => /^[a-z A-Z -']+$/, :message => "Invalid Last Name"
+   validates_format_of :last_name, :with => /^[a-z A-Z \- -']+$/, :message => "Invalid Name"
   validates_format_of :major, :with => /^[a-z A-Z -']+$/, :message => "Invalid Major"
-  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(qatar\.)?(cmu|vcu|tamu|georgetown)\.(edu)$/i, :message => "not vaild email" 
+  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(qatar\.|qatar-med\.)?(cmu|vcu|tamu|georgetown|cornell)\.(edu)$/i, :message => "not vaild email" 
   validates_format_of :mobile, :with => /^(\+?\d{8}|\+?\d{3}?[-.]\d{4}?[-.]\d{4})$/, :message => "should be 8 digits"
 
   def proper_name
