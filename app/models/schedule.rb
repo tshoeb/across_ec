7cdd1b605a87c052925579c60504af658e2acd5b
Talal Shoeb <talal@cmu.edu>
2013-04-21 22:44:36 +0300

    fixin uploading

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -5,7 +5,7 @@ class Schedule < ActiveRecord::Base
   attr_accessible :date_uploaded, :file_name, :schedule, :semester_year, :registrar_id
   mount_uploader :schedule, ScheduleUploader
   belongs_to :registrar
-  validates :file_name, :date_uploaded, :semester_year, :presence => true  
+  validates :file_name, :semester_year, :presence => true  
   
   def setting_date
 	self.date_uploaded = Date.today
